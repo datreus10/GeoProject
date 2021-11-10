1fe74f045d0713f1fd1a7b4a4f4946a17cb70b21
Đạt Nguyễn Thành <18520580@ms.uit.edu.vn>
2021-11-10 15:08:56 +0700

    update

--- test.js
+++ test.js
@@ -1,9 +1,29 @@
+const revarr = arr => {
+    if (arr.length == 1) return arr[0]
+    return arr.map(e => revarr(e))
+}
+
 const getdata = async (url) => {
     const response = await fetch(url);
     var data = await response.json();
-    data.elements.forEach(e => {
-            document.querySelector("#text").innerHTML += `[${e.lon},${e.lat}],`
-    })
+    const inputData = data[0].geojson.coordinates
+    const outData = revarr(inputData)
+    return inputData.length == 1 ? [outData] : outData
+
 }
 
-getdata("https://www.openstreetmap.org/api/0.6/relation/12340803/full.json")
+const getResult = async keywords => {
+    let result = []
+    for (const keyword of keywords) {
+        result = result.concat(await getdata(`https://nominatim.openstreetmap.org/search.php?q=${keyword}&polygon_geojson=1&format=json`))
+    }
+    document.querySelector("#text").innerHTML = JSON.stringify(result)
+}
+
+
+const keywords = ["Can Tho city"];
+getResult(keywords)
+
+// link code pen: https://codepen.io/datreus10/pen/RwZyjLW
+
+
